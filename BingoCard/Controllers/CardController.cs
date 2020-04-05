b9204837e0d82fd70c438cfccb4ff447b5e26489
xavieroldan <xavieroldan@gmail.com>
2020-04-05 10:33:14 +0200

    New random features

--- BingoCard/Controllers/CardController.cs
+++ BingoCard/Controllers/CardController.cs
@@ -19,7 +19,7 @@ namespace BingoCard.Controllers
         // GET: Card  
         public ActionResult Index()
         {
-            int[,] columnMargins = new int[9, 2] { { 1, 9 }, { 10, 19 }, { 20, 29 }, { 30, 39 }, { 40, 49 }, { 50, 59 }, { 60, 69 }, { 70, 79 }, { 80, 90 } };            
+            int[,] columnMargins = new int[9, 2] { { 1, 9 }, { 10, 19 }, { 20, 29 }, { 30, 39 }, { 40, 49 }, { 50, 59 }, { 60, 69 }, { 70, 79 }, { 80, 90 } };
             int[][] columnsArray = new int[9][];
             int[][] linesArray = new int[3][];
             int contColumns = 0;
@@ -27,7 +27,7 @@ namespace BingoCard.Controllers
             //Create the 27 numbers of the card
             //Columns generation
             for (int i = 0; i < 9; i++)
-            {              
+            {
                 var columnNumbers = FillCard(3, columnMargins[i, 0], columnMargins[i, 1]);
                 columnsArray[contColumns] = columnNumbers;
                 contColumns++;
@@ -38,7 +38,7 @@ namespace BingoCard.Controllers
             {
                 int[] line = new int[9];
                 for (int j = 0; j < 9; j++)
-                {                    
+                {
                     line[j] = columnsArray[j][i];
                 }
                 linesArray[i] = line;
@@ -48,24 +48,47 @@ namespace BingoCard.Controllers
             //Blanks template generation 
 
             int[][,] blanksTemplates =
-                new int [][,]
-                { 
+                new int[][,]
+                {
                     new int[,] {{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 1, 1, 1, 1, 0, 1, 0, 0, 0 } },
                     new int[,] {{ 0, 1, 0, 1, 1, 0, 0, 1, 1 },{ 0, 1, 1, 0, 0, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 } },
-                    new int[,] {{ 1, 0, 0, 0, 1, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 },{ 1, 0, 1, 0, 1, 0, 1, 0, 1 } } 
-                };              
+                    new int[,] {{ 1, 0, 0, 0, 1, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 },{ 1, 0, 1, 0, 1, 0, 1, 0, 1 } }
+                };
 
             //Try one random
             var templateId = rnd.Next(0, blanksTemplates.Length - 1);
 
             //Apply changes
-            for (int i = 0; i < 3; i++)
+            //Random lines order
+
+            int[][] rndLines = new int [][]
+                {
+                    new int [] { 0, 1, 2 },
+                    new int [] { 1, 0, 2 },
+                    new int [] { 1, 2, 0 },
+                    new int [] { 2, 1, 0 },
+                    new int [] { 2, 0, 1 },
+                    new int [] { 0, 2, 1 } 
+                };
+
+            var order = rndLines[rnd.Next(0,6)];
+
+            for (int k = 0; k < 3; k++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    linesArray[i][j] *= blanksTemplates[templateId][i, j];
+                    linesArray[order[k]] [j] *= blanksTemplates[templateId][order[k], j];
                 }
             }
+            
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 9; j++)
+            //    {
+            //        linesArray[i][j] *= blanksTemplates[templateId][i, j];
+            //    }
+            //}
 
             //Count the squares in a row for a line
             //Return the card  
