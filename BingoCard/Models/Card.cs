e6b047801f084853037dd23a8e9665c63cf6128b
xavieroldan <xavieroldan@gmail.com>
2020-04-06 17:47:57 +0200

    Line detection fisrt steps

--- BingoCard/Models/Card.cs
+++ BingoCard/Models/Card.cs
@@ -7,23 +7,16 @@ namespace BingoCard.Models
 {
     public class Card
     {
-        private Guid Id { get; set; }
+        public Guid Id { get; }
         public Guid PlayerId { get; set; }
-        public List<Square> Squares { get; set; }
-        //public Square[] Line1 { get; set; }
-        //public Square[] Line2 { get; set; }
-        //public Square[] Line3 { get; set; }
         public Square [,] Lines { get; set; }
         public bool IsLine { get; set; }
         public bool IsBingo { get; set; }
+        public int [] MarkedSquares { get; set; }
 
         public Card()
         {
             this.Id = Guid.NewGuid();
-            //this.Line1 = new Square[9];
-            //this.Line2 = new Square[9];
-            //this.Line3 = new Square[9];
-            this.Squares = new List<Square>();
             this.Lines = new Square[3,9]
                         {
                             { new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square(), new Square() },
@@ -32,6 +25,7 @@ namespace BingoCard.Models
                         };
             this.IsLine = false;
             this.IsBingo = false;
+            this.MarkedSquares = new int[] { 0, 0, 0 };
         }
     }
 }
