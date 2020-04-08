c42c3e8ca66facbbd443b11a28e59f164ae0980f
xavieroldan <xavieroldan@gmail.com>
2020-04-08 16:46:36 +0200

    Updated Nuggets + install EF

---
+++ BingoCard/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BingoCard.Models
+{
+    public class Player
+    {
+        public Player(Guid id, string name, int bingoCount, int lineCount, Guid cardId)
+        {
+            Id = Guid.NewGuid();
+            Name = "";
+            BingoCount = 0;
+            LineCount = 0;
+        }
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public int BingoCount { get; set; }
+        public int LineCount { get; set; }
+        public Guid CardId { get; set; }    
+    }
+}
