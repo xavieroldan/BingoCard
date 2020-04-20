370edf8eb8091f5f3710bd59e8f63ca321e0fdf2
xavieroldan <xavieroldan@gmail.com>
2020-04-20 17:19:03 +0200

    Added new DB

--- BingoCard/Models/Player.cs
+++ BingoCard/Models/Player.cs
@@ -7,9 +7,19 @@ namespace BingoCard.Models
 {
     public class Player
     {
+        public Player()
+        {
+            Id = new Guid();
+            BingoCount = 0;
+            LineCount = 0;
+            IsEnabled = true;
+        }
+
         public Guid Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public int BingoCount { get; set; }
         public int LineCount { get; set; }
+        public int AvatarID { get; set; }
+        public bool IsEnabled { get; set; }
     }
 }
