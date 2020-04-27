e23e3ab3b49dee427fa11b0cd220161034bcde09
xavieroldan <xavieroldan@gmail.com>
2020-04-27 17:50:51 +0200

    Created Helper folder and refine code of tjhe Card Controller

--- BingoCard/Controllers/CardController.cs
+++ BingoCard/Controllers/CardController.cs
@@ -1,117 +1,20 @@
 ﻿using BingoCard.DataAccess;
+using BingoCard.Helpers;
 using BingoCard.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BingoCard.Controllers
-{    
+{
     public class CardController : Controller
     {
-        private BingoCardDBContext db = new BingoCardDBContext();
-
-        private static readonly Random rnd;
-        static CardController()
-        {
-            rnd = new Random();
-        }
+        private BingoCardDBContext db = new BingoCardDBContext();      
 
         // GET: Card  
         public ActionResult Index()
         {
-            int[,] columnMargins = new int[9, 2] { { 1, 9 }, { 10, 19 }, { 20, 29 }, { 30, 39 }, { 40, 49 }, { 50, 59 }, { 60, 69 }, { 70, 79 }, { 80, 90 } };
-            int[][] columnsArray = new int[9][];
-            int[][] linesArray = new int[3][];
-            int contColumns = 0;
-
-            //Create the 27 numbers of the card
-            //Columns generation
-            for (int i = 0; i < 9; i++)
-            {
-                var columnNumbers = FillCard(3, columnMargins[i, 0], columnMargins[i, 1]);
-                columnsArray[contColumns] = columnNumbers;
-                contColumns++;
-            }
-
-            //Lines generation
-            for (int i = 0; i < 3; i++)
-            {
-                int[] line = new int[9];
-                for (int j = 0; j < 9; j++)
-                {
-                    line[j] = columnsArray[j][i];
-                }
-                linesArray[i] = line;
-            }
-
-            //Add the blank spaces
-            //Blanks template generation 
-
-            int[][,] blanksTemplates =
-                new int[][,]
-                {
-                    new int[,] {{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 1, 1, 1, 1, 0, 1, 0, 0, 0 } },
-                    new int[,] {{ 0, 1, 0, 1, 1, 0, 0, 1, 1 },{ 0, 1, 1, 0, 0, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 } },
-                    new int[,] {{ 1, 0, 0, 0, 1, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 },{ 1, 0, 1, 0, 1, 0, 1, 0, 1 } }
-                };
-
-            //Try one random
-            var templateId = rnd.Next(0, blanksTemplates.Length - 1);
-
-            //Apply changes
-            //Random lines order
-
-            int[][] rndLines = new int [][]
-                {
-                    new int [] { 0, 1, 2 },
-                    new int [] { 1, 0, 2 },
-                    new int [] { 1, 2, 0 },
-                    new int [] { 2, 1, 0 },
-                    new int [] { 2, 0, 1 },
-                    new int [] { 0, 2, 1 } 
-                };
-
-            var order = rndLines[rnd.Next(0,6)];
-
-            for (int k = 0; k < 3; k++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    linesArray[order[k]] [j] *= blanksTemplates[templateId][order[k], j];
-                }
-            }
-
-            //Return an object Card
-
-            Card myCard = new Card();
-
-            for (int i = 0; i < 3; i++)
-            {
-                
-                for (int j = 0; j < 9; j++)
-                {
-                    Square mySquare = new Square
-                    {
-                        Row = i,
-                        Column = j
-                    };
-                    int number = linesArray[i][j];
-                    if (number == 0){ mySquare.IsBlank = true; }
-                    else
-                    {
-                        mySquare.IsBlank = false;
-                        mySquare.Number = number;
-                    }
-                    myCard.Lines[i,j] = mySquare;
-                }
-            }
-
-            var output = myCard;
-            return View(output);
+            return View(CardHelper.CreateCard());
         }
 
         // GET: Card  Id Player
@@ -128,146 +31,18 @@ namespace BingoCard.Controllers
                 return HttpNotFound();
             }
 
-            int[,] columnMargins = new int[9, 2] { { 1, 9 }, { 10, 19 }, { 20, 29 }, { 30, 39 }, { 40, 49 }, { 50, 59 }, { 60, 69 }, { 70, 79 }, { 80, 90 } };
-            int[][] columnsArray = new int[9][];
-            int[][] linesArray = new int[3][];
-            int contColumns = 0;
-
-            //Create the 27 numbers of the card
-            //Columns generation
-            for (int i = 0; i < 9; i++)
-            {
-                var columnNumbers = FillCard(3, columnMargins[i, 0], columnMargins[i, 1]);
-                columnsArray[contColumns] = columnNumbers;
-                contColumns++;
-            }
-
-            //Lines generation
-            for (int i = 0; i < 3; i++)
-            {
-                int[] line = new int[9];
-                for (int j = 0; j < 9; j++)
-                {
-                    line[j] = columnsArray[j][i];
-                }
-                linesArray[i] = line;
-            }
-
-            //Add the blank spaces
-            //Blanks template generation 
-
-            int[][,] blanksTemplates =
-                new int[][,]
-                {
-                    new int[,] {{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 1, 1, 1, 1, 0, 1, 0, 0, 0 } },
-                    new int[,] {{ 0, 1, 0, 1, 1, 0, 0, 1, 1 },{ 0, 1, 1, 0, 0, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 } },
-                    new int[,] {{ 1, 0, 0, 0, 1, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 },{ 1, 0, 1, 0, 1, 0, 1, 0, 1 } }
-                };
-
-            //Try one random
-            var templateId = rnd.Next(0, blanksTemplates.Length - 1);
-
-            //Apply changes
-            //Random lines order
-
-            int[][] rndLines = new int[][]
-                {
-                    new int [] { 0, 1, 2 },
-                    new int [] { 1, 0, 2 },
-                    new int [] { 1, 2, 0 },
-                    new int [] { 2, 1, 0 },
-                    new int [] { 2, 0, 1 },
-                    new int [] { 0, 2, 1 }
-                };
-
-            var order = rndLines[rnd.Next(0, 6)];
-
-            for (int k = 0; k < 3; k++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    linesArray[order[k]][j] *= blanksTemplates[templateId][order[k], j];
-                }
-            }
-
             //Return an object Card
-            Card myCard = new Card();
+            Card myCard = CardHelper.CreateCard();
 
             //Add the player Id
-            myCard.PlayerId = player.Id;
-
-            for (int i = 0; i < 3; i++)
-            {
-
-                for (int j = 0; j < 9; j++)
-                {
-                    Square mySquare = new Square
-                    {
-                        Row = i,
-                        Column = j
-                    };
-                    int number = linesArray[i][j];
-                    if (number == 0) { mySquare.IsBlank = true; }
-                    else
-                    {
-                        mySquare.IsBlank = false;
-                        mySquare.Number = number;
-                    }
-                    myCard.Lines[i, j] = mySquare;
-                }
-            }
+            myCard.PlayerId = player.Id;           
 
             return View(myCard);
         }
-
-        private int[] FillCard(int squares, int min = 1, int max = 90)
-        {
-            int[] listNum = new int[squares];
-            //var listNum = input;
-            int num = 0;
-            bool isEqualExist = false;
-            bool isExiting = false;
-
-            for (int i = 0; i < squares; i++)
-            {
-                while (!isExiting)
-                {
-                    num = rnd.Next(min, max);
-                    foreach (var item in listNum)
-                    {
-                        if (item == num)
-                        {
-                            isEqualExist = true;
-                            break;
-                        }
-                    }
-                    if (!isEqualExist) isExiting = true;
-                    else isEqualExist = false;
-                }
-                listNum [i] = num;
-                isExiting = false;
-            }
-            return listNum;
-        }
-
         [HttpPost]
         public ActionResult PostCard(Card card)
         {
             return Json(new { url = "URL" });
-        }
-
-        //public ActionResult PlayerCard(Guid? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Player player = db.Players.Find(id);
-        //    if (player == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(player);
-        //}
+        }       
     }
 }
---
+++ BingoCard/Helpers/CardHelper.cs
@@ -0,0 +1,135 @@
+﻿using BingoCard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BingoCard.Helpers
+{
+    public class CardHelper
+    {
+        private static readonly Random rnd;
+        static CardHelper()
+        {
+            rnd = new Random();
+        }
+        private static int[] RndNumbersCardgenerator(int squares, int min = 1, int max = 90)
+        {
+            int[] listNum = new int[squares];
+            int num = 0;
+            bool isEqualExist = false;
+            bool isExiting = false;
+
+            for (int i = 0; i < squares; i++)
+            {
+                while (!isExiting)
+                {
+                    num = rnd.Next(min, max);
+                    foreach (var item in listNum)
+                    {
+                        if (item == num)
+                        {
+                            isEqualExist = true;
+                            break;
+                        }
+                    }
+                    if (!isEqualExist) isExiting = true;
+                    else isEqualExist = false;
+                }
+                listNum[i] = num;
+                isExiting = false;
+            }
+            return listNum;
+        }
+        public static Card CreateCard()
+        {
+            int[,] columnMargins = new int[9, 2] { { 1, 9 }, { 10, 19 }, { 20, 29 }, { 30, 39 }, { 40, 49 }, { 50, 59 }, { 60, 69 }, { 70, 79 }, { 80, 90 } };
+            int[][] columnsArray = new int[9][];
+            int[][] linesArray = new int[3][];
+            int contColumns = 0;
+
+            //Create the 27 numbers of the card
+            //Columns generation
+            for (int i = 0; i < 9; i++)
+            {
+                var columnNumbers = RndNumbersCardgenerator(3, columnMargins[i, 0], columnMargins[i, 1]);
+                columnsArray[contColumns] = columnNumbers;
+                contColumns++;
+            }
+
+            //Lines generation
+            for (int i = 0; i < 3; i++)
+            {
+                int[] line = new int[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    line[j] = columnsArray[j][i];
+                }
+                linesArray[i] = line;
+            }
+
+            //Add the blank spaces
+            //Blanks template generation 
+
+            int[][,] blanksTemplates =
+                new int[][,]
+                {
+                    new int[,] {{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 0, 1, 0, 0, 1, 1, 0, 1, 1 },{ 1, 1, 1, 1, 0, 1, 0, 0, 0 } },
+                    new int[,] {{ 0, 1, 0, 1, 1, 0, 0, 1, 1 },{ 0, 1, 1, 0, 0, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 } },
+                    new int[,] {{ 1, 0, 0, 0, 1, 1, 1, 0, 1 },{ 1, 1, 0, 1, 1, 0, 0, 1, 0 },{ 1, 0, 1, 0, 1, 0, 1, 0, 1 } }
+                };
+
+            //Try one random
+            var templateId = rnd.Next(0, blanksTemplates.Length - 1);
+
+            //Apply changes
+            //Random lines order
+
+            int[][] rndLines = new int[][]
+                {
+                    new int [] { 0, 1, 2 },
+                    new int [] { 1, 0, 2 },
+                    new int [] { 1, 2, 0 },
+                    new int [] { 2, 1, 0 },
+                    new int [] { 2, 0, 1 },
+                    new int [] { 0, 2, 1 }
+                };
+
+            var order = rndLines[rnd.Next(0, 6)];
+
+            for (int k = 0; k < 3; k++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    linesArray[order[k]][j] *= blanksTemplates[templateId][order[k], j];
+                }
+            }
+
+            //Return an object Card
+
+            Card myCard = new Card();
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 9; j++)
+                {
+                    Square mySquare = new Square
+                    {
+                        Row = i,
+                        Column = j
+                    };
+                    int number = linesArray[i][j];
+                    if (number == 0) { mySquare.IsBlank = true; }
+                    else
+                    {
+                        mySquare.IsBlank = false;
+                        mySquare.Number = number;
+                    }
+                    myCard.Lines[i, j] = mySquare;
+                }
+            }
+            return myCard;
+        }
+    }
+}
